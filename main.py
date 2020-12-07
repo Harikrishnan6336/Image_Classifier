f0a2b8a2c00c8fedc7d10677da4b7ecdf5932b30
Hari Krishnan U <harikrishnan6336@cet.ac.in>
2020-12-07 10:10:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,14 +18,16 @@ data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 labels = gen_labels()
 
 while True:
-
+    # Choose a suitable font
     font = cv2.FONT_HERSHEY_SIMPLEX
     ret, frame = image.read()
     frame = cv2.flip(frame, 1)
+    # In case the image is not read properly
     if not ret:
         continue
-
+    # Draw a rectangle, in the frame
     frame = cv2.rectangle(frame, (220, 80), (530, 360), (0, 0, 255), 3)
+    # Draw another rectangle in which the image to labelled is to be shown.
     frame2 = frame[80:360, 220:530]
     # resize the image to a 224x224 with the same strategy as in TM2:
     # resizing the image to be at least 224x224 and then cropping from the center
@@ -39,12 +41,15 @@ while True:
     pred = model.predict(data)
     result = np.argmax(pred[0])
 
+    # Print the predicted label into the screen.
     cv2.putText(frame,  "Label : " +
                 labels[str(result)], (280, 400), font, 1, (0, 255, 0), 2, cv2.LINE_AA)
 
+    # Exit, when 'q' is pressed on the keyboard
     if cv2.waitKey(1) and 0xff == ord('q'):
         exit = True
         break
+    # Show the frame   
     cv2.imshow('Frame', frame)
 
 image.release()
