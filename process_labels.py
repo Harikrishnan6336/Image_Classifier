c319250c14fe5ab1d9a18bc62ae7566f1215f689
Harikrishnan6336 <harikrishnan6336@cet.ac.in>
2020-12-07 15:52:32 +0530

    Update process_labels.py

--- process_labels.py
+++ process_labels.py
@@ -1,11 +1,12 @@
 # Generates a dict to store the labels(keys) and its names(values)
 
 def gen_labels():
-    labels = {}
-    with open("labels.txt", "r") as label:
-        text = label.read()
-        lines = text.split("\n")
-        for line in lines:
-            hold = line.split(" ", 1)
-            labels[hold[0]] = hold[1]
-    return labels
+        labels = {}
+        with open("labels.txt", "r") as label:
+            text = label.read()
+            lines = text.split("\n")
+            print(lines)
+            for line in lines[0:-1]:
+                    hold = line.split(" ", 1)
+                    labels[hold[0]] = hold[1]
+        return labels
